704178669bea7267c9263691b062f6ce1b4a2f37
Ukaypromise <promiseukaimo@gmail.com>
2023-03-29 00:51:00 +0100

    Update app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation)
+      u.permit(:fullName, :lastName, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      u.permit(:fullName, :lastName, :email, :password, :password_confirmation, :current_password)
     end
   end
 
