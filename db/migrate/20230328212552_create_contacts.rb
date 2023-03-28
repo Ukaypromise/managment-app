164128d3c2d6d1127eb59a0ab938fc6a11e56879
Ukaypromise <promiseukaimo@gmail.com>
2023-03-28 22:28:00 +0100

    Add contact model and controller

---
+++ db/migrate/20230328212552_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.integer :info
+
+      t.timestamps
+    end
+  end
+end
